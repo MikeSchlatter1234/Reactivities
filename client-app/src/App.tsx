8c737bf514dcce54496658677a300645ebfa8930
Mike Schlatter <xmes@yahoo.com>
2020-04-30 16:34:06 -0500

    End Stg 3

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,14 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import { Header, Icon, List } from 'semantic-ui-react'
 import './App.css';
+import axios from 'axios';
 
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-<h1>hello world</h1>>
+
+class App extends Component {
+  state = {
+    values:[]
+  }
+
+  componentDidMount(){
+    axios.get('http://localhost:5000/api/values')
+    .then((response)=>{
+      this.setState({
+        values: response.data
+      })
+    
+    })
+  }
+
+  render() {
+    return (
+      <div >
+        <Header as='h2'>
+          <Icon name='users' />
+          <Header.Content>reactivities</Header.Content>
+      </Header>
+      <List>
+          {this.state.values.map((value: any)  =>(
+            <List.Item key={value.id}>{ value.name }</List.Item>
+          ))}
+      </List>
     </div>
-  );
+    );
+  }
+
 }
 
 export default App;
