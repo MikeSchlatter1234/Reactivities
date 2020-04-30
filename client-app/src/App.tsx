6eca093ec9a9509cb9bc41448e00aa2bdcabe41b
Mike Schlatter <xmes@yahoo.com>
2020-04-30 15:25:41 -0500

    Add React

---
+++ client-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+<h1>hello world</h1>>
+    </div>
+  );
+}
+
+export default App;
