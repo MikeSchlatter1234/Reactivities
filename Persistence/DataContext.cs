a37a81c4826d1e8e8bf275627b90bcb4cad1eea8
Mike Schlatter <xmes@yahoo.com>
2020-05-01 11:48:42 -0500

    End of Section 4

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Domain;
+﻿using Domain;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence
@@ -11,7 +10,9 @@ namespace Persistence
             
         }
 
+        //need to run dotnet ef migrations add "name" -[ Persistence/ -s API/
        public DbSet<Value> Values { get; set; }
+       public DbSet<Activity> Activities{get; set;}
 
        protected override void OnModelCreating(ModelBuilder builder)
        {
